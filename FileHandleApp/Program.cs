a105656ed2c6ef2b3a3e54ac5b4f3f45d4277a86
Sebestyen Orsi <elfromka@gmail.com>
2019-01-30 21:17:58 +0200

    creating methods and adding Exception

--- FileHandleApp/Program.cs
+++ FileHandleApp/Program.cs
@@ -11,6 +11,58 @@ namespace FileHandleApp
     {
         static void Main(string[] args)
         {
+            //FileWriter();
+            //FileReader();
+            CheckForFile();
+        }
+
+        private static void CheckForFile()
+        {
+            if (File.Exists("numbers.txt"))
+            {
+                Console.WriteLine("The file with numbers exists!");
+                FileReader();
+            }
+            else
+            {
+                throw new Exception("The file is missing!");
+            }
+        }
+
+        // reading the file
+        private static void FileReader(string fileName = "numbers.txt")
+        {
+            using (StreamReader sr = new StreamReader(fileName))
+            {
+                string line;
+
+                while ((line = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+
+                Console.ReadKey();
+            }
+           
+        }
+
+        // writing in the file
+        private static void FileWriter()
+        {
+            var names = new string[] { "Orsi", "Csharp" };
+            string fileName = "numbers.txt";
+
+            // append true to not overwrite the existing data in the file
+            // just for test
+            using (StreamWriter sw = new StreamWriter(fileName, true))
+            {
+                foreach (var name in names)
+                {
+                    sw.WriteLine(name);
+                }
+            }
         }
     }
 }
+
+   
